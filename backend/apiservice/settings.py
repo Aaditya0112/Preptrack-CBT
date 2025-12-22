8d928dca7ce3dedc718a09e7bc9cb8edb281b6b4
Aaditya0112 <rajaaditya0112@gmail.com>
2025-12-22 12:33:31 +0530

    feat: update settings to use environment variables and add sample .env file

--- backend/apiservice/settings.py
+++ backend/apiservice/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8b&(u))xd!&uh6lwy!pgvi14)m#^yoj&l)-uvsz6()xz$s23j%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -52,7 +53,7 @@ REST_FRAMEWORK = {
 
 }
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    config('CORS_ALLOWED_ORIGINS')
 ]           
 
 
@@ -94,10 +95,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "preptrack_db2",
-        "USER": "postgres",
-        "PASSWORD": "A@di2345",
+        "USER": config('DATABASE_USER'),
+        "PASSWORD": config('DATABASE_PASSWORD'),
         "HOST": "localhost",
-        "PORT": "5433",
+        "PORT": config('DATABASE_PORT', cast=int),
     }
 }
 
