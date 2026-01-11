080b296c0f662cc9f8334c5d0733eac3f445a101
Aaditya0112 <rajaaditya0112@gmail.com>
2026-01-11 23:01:13 +0530

    feat: TEMPORARY SOLUTION - add media view and update URL routing for media files

--- backendWithAuth/backendWithAuth/urls.py
+++ backendWithAuth/backendWithAuth/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import media_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/assessments/', include('assessments.urls')),
     path('api/performance/', include('performance.urls')),
+    re_path(r'^media/(?P<path>.*)$', media_view, name='media'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
